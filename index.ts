74deff496aa8799f9296f35dd04d3e494d35ebf4
Lorenzo Murarotto <lnzmrr@gmail.com>
2022-02-09 20:48:56 +0100

    fix errors

--- index.ts
+++ index.ts
@@ -1,6 +1,3 @@
 import "./lib/vpc";
 import "./lib/server";
-import { eip, hostedZone } from "./lib/dns";
-
-export const nameServers = hostedZone.nameServers.apply((ns) => ns.join(", "));
-export const host = eip.address;
+import "./lib/dns";
