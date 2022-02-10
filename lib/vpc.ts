9568ff5855d1ea10c44e0a02b01f781ecbe16d9b
Lorenzo Murarotto <lnzmrr@gmail.com>
2022-02-10 10:44:15 +0100

    allow outgoing internet

--- lib/vpc.ts
+++ lib/vpc.ts
@@ -8,3 +8,6 @@ awsx.ec2.SecurityGroupRule.ingress("ping", securityGroup, new awsx.ec2.AnyIPv4Lo
 awsx.ec2.SecurityGroupRule.ingress("ssh", securityGroup, new awsx.ec2.AnyIPv4Location(), new awsx.ec2.TcpPorts(22));
 awsx.ec2.SecurityGroupRule.ingress("http", securityGroup, new awsx.ec2.AnyIPv4Location(), new awsx.ec2.TcpPorts(80));
 awsx.ec2.SecurityGroupRule.ingress("https", securityGroup, new awsx.ec2.AnyIPv4Location(), new awsx.ec2.TcpPorts(443));
+
+awsx.ec2.SecurityGroupRule.egress("ipv4", securityGroup, new awsx.ec2.AnyIPv4Location(), new awsx.ec2.AllTraffic());
+awsx.ec2.SecurityGroupRule.egress("ipv6", securityGroup, new awsx.ec2.AnyIPv6Location(), new awsx.ec2.AllTraffic());
