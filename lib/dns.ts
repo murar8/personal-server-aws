c8174eebb40ccfe1a8cd4d93d5210882d9f70028
Lorenzo Murarotto <lnzmrr@gmail.com>
2022-02-10 22:13:10 +0100

    improve style

--- lib/dns.ts
+++ lib/dns.ts
@@ -7,14 +7,12 @@ const config = new pulumi.Config("network");
 const domain = config.require("domain");
 const subdomain = config.require("subdomain");
 
-const fqdn = subdomain + "." + domain;
-
 const hostedZone = aws.route53.getZone({ name: domain, privateZone: false });
 
 export const eip = new aws.ec2.Eip("server-eip", { vpc: true, instance: spotRequest.spotInstanceId });
 
 export const dnsRecord = new aws.route53.Record("server-record", {
-    name: fqdn,
+    name: subdomain + "." + domain,
     records: [eip.publicIp],
     zoneId: pulumi.output(hostedZone).apply((z) => z.id),
     type: "A",
